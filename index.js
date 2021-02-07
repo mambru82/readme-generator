b51b27315fd77ba6e6ba35b9f18541b571b67e8c
mambru <ecastro82@gmail.com>
2021-02-06 21:45:35 -0600

    removed commented out code

--- index.js
+++ index.js
@@ -195,20 +195,3 @@ promptUser()
         console.log(readmePage);
         return writeFile(readmePage);
     })
-    // .then(console.log(projectData))
-    // .then(portfolioData => {
-    //     return generatePage(portfolioData);
-    // })
-    // .then(pageHTML => {
-    //     return writeFile(pageHTML);
-    // })
-    // .then(writeFileResponse => {
-    //     console.log(writeFileResponse);
-    //     return copyFile();
-    // })
-    // .then(copyFileResponse => {
-    //     console.log(copyFileResponse);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // });
